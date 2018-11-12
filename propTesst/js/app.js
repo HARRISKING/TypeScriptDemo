8d850f1ff0dfe2921a349f8629f86869cc7fc657
HARRISKING <llc13588440410@outlook.com>
2018-11-12 17:29:44 +0800

    update

---
+++ propTesst/js/app.js
@@ -0,0 +1,18 @@
+"use strict";
+function a(name) {
+    console.log('引用成功', name);
+}
+// document.getElementsByClassName('root').addEventListener('click',a,false)
+var user = [1, 223, 1];
+var obj = {
+    a: 234,
+};
+var info = {
+    name: 'harrisking',
+    age: 25,
+    new: 4545
+};
+var arr = [12, 3, 3, '2', 2, 2];
+console.log('arr', arr);
+var newconfig = 'ssss';
+var newconfig1 = 'wsrfddsfdsf';
