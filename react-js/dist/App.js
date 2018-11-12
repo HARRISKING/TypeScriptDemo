d89da566e5c58251a272fc3f4f6e7df6b48de73c
harrisking <harrisking@harriskingdeMacBook-Pro.local>
2018-11-12 22:36:21 +0800

    update

---
+++ react-js/dist/App.js
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+class App extends Component {
+    render() {
+        return className = "App" >
+            dfd
+            < /div>;
+        ;
+    }
+}
+export default App;
